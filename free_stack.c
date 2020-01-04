064a9eadd86cbc148dfa0a6920fdfbc9b516e070
oscar <oscarnetworkingpro@gmail.com>
2020-01-04 18:24:58 -0500

    Betty freee

--- free_stack.c
+++ free_stack.c
@@ -1,15 +1,19 @@
 #include "monty.h"
 
+/**
+ * free_stack - free of the list
+ * @stack: Head of stack
+ */
 void free_stack(stack_t **stack)
 {
-    stack_t *aux;
+	stack_t *aux;
 
-    while (*stack)
-    {
-        aux = (*stack)->next;
-        free(*stack);
-        aux->prev = NULL;
-        *stack = aux;
-    }
-    free(*stack);
+	while (*stack)
+	{
+		aux = (*stack)->next;
+		free(*stack);
+		aux->prev = NULL;
+		*stack = aux;
+	}
+	free(*stack);
 }
