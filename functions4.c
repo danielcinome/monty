20ef3e247e15d58374dc1815e03c82594eecd7f2
danielcinome <danielchinome987@gmail.com>
2020-01-06 19:06:00 +0000

    add function rotr

---
+++ functions4.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * rotr - rotates the stack to the bottom.
+ * @stack : head
+ * @line_number : line number
+ */
+
+void rotr(stack_t **stack, unsigned int line_number)
+{
+	int val = 0;
+
+	UNUSED(line_number);
+	if ((*stack) != NULL && (*stack)->next != NULL)
+	{
+		while (*stack)
+		{
+			*stack = (*stack)->next;
+		}
+		val = (*stack)->n;
+		while ((*stack)->prev != NULL)
+		{
+			(*stack)->n = (*stack)->prev->n;
+			*stack = (*stack)->prev;
+		}
+		(*stack)->n = val;
+	}
+}
